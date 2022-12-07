b70a111f4b54fc7c50b1eb0a80b3f675933e5ba7
Gaiya M. Obed <95748043+gaiyaobed@users.noreply.github.com>
2022-12-07 18:09:52 +0100

    fetch all books

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DashboardService} from "./dashboard.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor( private readonly dashboardService: DashboardService) { }
 
   ngOnInit(): void {
+    this.getBooks()
   }
 
+  loading: boolean =false
+  books: any = []
+
+
+  getBooks(): void{
+    this.loading = true
+    this.dashboardService.getBooks()
+      .subscribe({
+        next: (response) => {
+          this.loading = false
+          this.books = response
+          console.log(this.books)
+        },
+        error: (error) => {
+          console.log('error er', error);
+          this.loading = false
+        },
+        complete: () => {
+          this.loading = false
+          console.log('Done fetching data')
+        }
+      });
+  }
 }
