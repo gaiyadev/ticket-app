dbd6bdbbd297138d448a97d7195261062890747c
Gaiya M. Obed <95748043+gaiyaobed@users.noreply.github.com>
2022-12-11 23:55:38 +0100

    big

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ButtonModule, CardModule, FormModule, GridModule} from "@coreui/angular";
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
@@ -38,10 +36,6 @@ import { WalletComponent } from './wallet/wallet.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CardModule,
-    GridModule,
-    FormModule,
-    ButtonModule,
     FormsModule,
     HttpClientModule
   ],
